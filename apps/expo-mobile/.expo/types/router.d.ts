68d2815cc3601f29f22403a2e44d1de7dc467137
sahilsohailofcl <sahilsohailofcl@gmail.com>
2025-12-05 22:40:22 +0000

    EF-2

--- apps/expo-mobile/.expo/types/router.d.ts
+++ apps/expo-mobile/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/..\..\next-web\.next\dev\types\app\(app)\challenges\page` | `/..\..\next-web\.next\dev\types\app\(app)\detox-path\page` | `/..\..\next-web\.next\dev\types\app\(app)\focus-mode\page` | `/..\..\next-web\.next\dev\types\app\(app)\habits\page` | `/..\..\next-web\.next\dev\types\app\(app)\mindfulness\page` | `/..\..\next-web\.next\dev\types\app\(app)\profile\page` | `/..\..\next-web\.next\dev\types\app\(app)\progress\page` | `/..\..\next-web\.next\dev\types\app\(app)\subscription\page` | `/_sitemap` | `/detox-path` | `/focus-mode`;
+      StaticRoutes: `/` | `/_sitemap` | `/detox-path` | `/focus-mode`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
